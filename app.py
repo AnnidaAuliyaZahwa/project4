52c302a95721c510daa16e9e38934a2057429f36
unknown <anidazahwa22@gmail.com>
2023-08-19 14:40:58 +0700

    flask setup for deployment

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
+import os
+from os.path import join,dirname
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
 from datetime import datetime
 
-connection_string ='mongodb+srv://AnnidaAuliyaZahwa:Annida276@cluster0.pniqosj.mongodb.net/?retryWrites=true&w=majority'
-client = MongoClient(connection_string)
-db = client.dbsparta
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME = os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
